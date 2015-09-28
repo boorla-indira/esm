1b7a54582b503f6790193b32cd7e232bf5af168d
Indira Boorla <boorla.indira@gmail.com>
2015-09-28 10:26:02 +0100

    DB script modified

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,22 +1,20 @@
-create table EMPLOYEE (
+create table esm.EMPLOYEE (
    employee_id BIGINT NOT NULL AUTO_INCREMENT,
    name VARCHAR(30) NOT NULL,
    department  VARCHAR(30) NOT NULL,
-   PRIMARY KEY (employee_id),
-   UNIQUE (sso_id)
+   PRIMARY KEY (employee_id)
 );
   
 create table SKILL(
    skill_id BIGINT NOT NULL AUTO_INCREMENT,
    name VARCHAR(30) NOT NULL,
-   PRIMARY KEY (skill_id),
-   UNIQUE (type)
+   PRIMARY KEY (skill_id)
 );
   
 CREATE TABLE EMPLOYEE_SKILL (
     employee_id BIGINT NOT NULL,
     skill_id BIGINT NOT NULL,
     PRIMARY KEY (employee_id , skill_id),
-    CONSTRAINT FK_EMPLOYEE FOREIGN KEY (employee_id) REFERENCES employee (employee_id),
+    CONSTRAINT FK_EMPLOYEE FOREIGN KEY (employee_id) REFERENCES EMPLOYEE (employee_id),
     CONSTRAINT FK_SKILL FOREIGN KEY (skill_id) REFERENCES SKILL (skill_id)
 );
